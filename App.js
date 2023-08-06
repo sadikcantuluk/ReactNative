f5aa99d7afb5c36ff9589c6575438a12453bbc25
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-08-06 20:17:59 +0300

    flex direction

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.box1}></View>
-      <View style={styles.box2}></View>
+      <View style={styles.box1}>
+      <View style={styles.child4}></View>
+        <View style={styles.child5}></View>
+        <View style={styles.child6}></View>
+      </View>
+      <View style={styles.box2}>
+        <View style={styles.child1}></View>
+        <View style={styles.child2}></View>
+        <View style={styles.child3}></View>
+      </View>
     </View>
   );
 }
@@ -14,13 +22,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'yellow',
+    flexDirection:"row"
   },
   box1:{
-    flex:2,
-    backgroundColor:"white"
+    flex:1,
+    backgroundColor:"white",
   },
   box2:{
     flex:1,
+    backgroundColor:"red",
+    flexDirection:"row"
+  },
+  child1:{
+    flex:1,
+    backgroundColor:"yellow"
+  },
+  child2:{
+    flex:1,
+    backgroundColor:"grey"
+  },
+  child3:{
+    flex:1,
+    backgroundColor:"green"
+  },
+  child4:{
+    flex:1,
     backgroundColor:"red"
+  },
+  child5:{
+    flex:1,
+    backgroundColor:"blue"
+  },
+  child6:{
+    flex:1,
+    backgroundColor:"white"
   }
 });
