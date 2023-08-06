6ecc9969b09fd195ce19bec2353cb08d4822e6e7
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-08-06 19:36:07 +0300

    styling implemented

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React </Text>
+      <Text>Hello React Native </Text>
       <StatusBar style="auto" />
+
+    <View style={[styles.box,styles.box1]}>
+      <Text style={styles.text}>ReactNative</Text>
+    </View>
+
+    <View style={[styles.box,styles.box2]}>
+      <Text style={styles.text}>Flutter</Text>
+    </View>
+
+    <View style={styles.box}>
+      <Text style={styles.text}>Kotlin</Text>
+    </View>
+
     </View>
   );
 }
@@ -17,4 +30,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    width:300,
+    height:200,
+    backgroundColor:"red",
+    marginBottom:10
+  },
+  box1:{
+    backgroundColor:"blue"
+  },
+  box2:{
+    backgroundColor:"bisque"
+  },
+  text: {
+    fontSize:30,
+    color:"black",
+    textAlign:'center',
+    backgroundColor:"grey"
+  }
 });
